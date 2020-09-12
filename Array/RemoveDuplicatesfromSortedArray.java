fb57fe49106371dc0d083f1cae65e195480c2449
Arun Kumar <58946415+programmerarunkumar@users.noreply.github.com>
2020-09-12 19:49:02 +0530

    Making the code proper indentation

--- Array/RemoveDuplicatesfromSortedArray.java
+++ Array/RemoveDuplicatesfromSortedArray.java
@@ -6,19 +6,15 @@ class Solution {
         if(length == 0 || length == 1){
             return length;
         }else{
-            
             int temp=nums[0];
             int originalLength=1;
             for(int i=1;i<length;i++){
-            
                 if(temp != nums[i]){
                     temp=nums[i];
                     nums[originalLength++]=nums[i];
                 }
-            
             }
             return originalLength;
-            
         }
         
     }
