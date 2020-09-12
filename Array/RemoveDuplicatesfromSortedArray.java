04b3cb3d0212dacde31c3555eedcafba5a36caa7
Arun Kumar <58946415+programmerarunkumar@users.noreply.github.com>
2020-09-12 19:45:54 +0530

    Create RemoveDuplicatesfromSortedArray.java

---
+++ Array/RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,30 @@
+class Solution {
+    
+    
+    
+    public int removeDuplicates(int[] nums) {
+        
+        int length=nums.length;
+        if(length == 0 || length == 1){
+            return length;
+        }else{
+            
+            int temp=nums[0];
+            int originalLength=1;
+            for(int i=1;i<length;i++){
+            
+                if(temp != nums[i]){
+                    temp=nums[i];
+                    nums[originalLength++]=nums[i];
+                }
+            
+            }
+            return originalLength;
+            
+        }
+        
+    }
+    
+
+    
+}
