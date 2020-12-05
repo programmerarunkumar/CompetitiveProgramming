ea3153df9ebe00f47401695595cf382080780894
Arun Kumar <58946415+programmerarunkumar@users.noreply.github.com>
2020-12-05 21:20:35 +0530

    Update TwoSum.java
    proper indentation

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -2,14 +2,12 @@
 class Solution {
     
     public int[] twoSum(int[] nums, int target) {
-        
         int value=0;
         int length = nums.length;
         int result[] = new int[2];
         
         HashMap<Integer,Integer>  map = new HashMap<>();
         for(int i=0;i<length;i++){
-          
             value=target-nums[i];
             if(map.containsKey(value)){
                 result[0]=i;
@@ -18,11 +16,9 @@ class Solution {
             }else{
                 map.put(nums[i],i);
             }
-
         }
         
         return result;
-        
     }
     
 }
