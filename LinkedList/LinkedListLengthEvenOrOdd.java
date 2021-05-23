109ce8acebc004f22d82b14cb7ed588d69be48e2
Arun Kumar <58946415+programmerarunkumar@users.noreply.github.com>
2021-05-23 14:01:41 +0530

    Create LinkedListLengthEvenOrOdd.java

---
+++ LinkedList/LinkedListLengthEvenOrOdd.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Node{
+  int data;
+  Node next;
+  Node(int d) {data = d; next = null; }
+}
+		
+public class GFG2{
+
+    Node head;  
+    
+    void printList(Node head){
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+       
+     Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 while(t-->0){
+		      	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++){
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+		        GFG obj=new GFG();
+			      int start=obj.isLengthEvenorOdd(head);
+		        if(start==0)
+		          System.out.println("Even");
+		        else
+		          System.out.println("Odd");
+		}
+    
+	}
+  
+}
+
+
+class GFG{
+    
+	int isLengthEvenorOdd(Node head){
+	    
+	    int length = 0;
+	    Node iterator = head;
+	    while(iterator != null){
+	        length++;
+	        iterator = iterator.next;
+	    }
+	    
+	    return (length%2) == 0 ? 0 : 1;
+	}
+	
+}
