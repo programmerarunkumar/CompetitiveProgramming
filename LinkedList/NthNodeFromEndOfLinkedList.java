44d7e03fe4fbb8dc5dba31459d8d85b93126e61d
Arun Kumar <58946415+programmerarunkumar@users.noreply.github.com>
2021-05-29 20:09:50 +0530

    Update NthNodeFromEndOfLinkedList.java

--- LinkedList/NthNodeFromEndOfLinkedList.java
+++ LinkedList/NthNodeFromEndOfLinkedList.java
@@ -10,58 +10,56 @@ class Node{
     }
 }
 	
-public class LinkedList_Element_From_Last{
-    
-    Node head;  
-	  Node tail;
-	  /* Function to print linked list */
-    void printList(Node head){
-        Node temp = head;
-        while (temp != null){
-           System.out.print(temp.data+" ");
-           temp = temp.next;
-        }  
-        System.out.println();
-    }
+
 	
- 
-    /* Inserts a new Node at front of the list. */
-    public void addToTheLast(Node node) {
-		  if (head == null) {
-			  head = node;
-			  tail = node;
-		  } else {
-		    tail.next = node;
-		    tail = node;
-		  }
+public class LinkedList_Element_From_Last{
+	
+	Node head;  
+	Node tail;
+	
+    	void printList(Node head){
+        	Node temp = head;
+        	while (temp != null)
+        	{
+           		System.out.print(temp.data+" ");
+           		temp = temp.next;
+        	}  
+        	System.out.println();
+    	}
+	
+    	public void addToTheLast(Node node) {
+		if (head == null) {
+			head = node;
+			tail = node;
+		} else {
+			tail.next = node;
+		    	tail = node;
+		}
     }
 	  
-     /* Drier program to test above functions */
     public static void main(String args[]){
-      Scanner sc = new Scanner(System.in);
-		  int t=sc.nextInt();
+	    
+         Scanner sc = new Scanner(System.in);
+	int t=sc.nextInt();
 		 
-		  while(t>0){
-		    int n = sc.nextInt();
-			  int k = sc.nextInt();
-			  LinkedList_Element_From_Last llist = new LinkedList_Element_From_Last();
-			  //int n=Integer.parseInt(br.readLine());
-			  int a1=sc.nextInt();
-			  Node head= new Node(a1);
-        llist.addToTheLast(head);
-        for (int i = 1; i < n; i++) {
-				  int a = sc.nextInt(); 
-				  llist.addToTheLast(new Node(a));
-			  }
-          
-		    //System.out.println(llist.head.data);
-        GfG g = new GfG(); 
-		    //System.out.println(k);
-		    System.out.println(g.getNthFromLast(llist.head,k));
-			  t--;
-		  }
-    }
-    
+	while(t>0)
+	{
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		LinkedList_Element_From_Last llist = new LinkedList_Element_From_Last();
+		int a1=sc.nextInt();
+		Node head= new Node(a1);
+            	llist.addToTheLast(head);
+            	for (int i = 1; i < n; i++) {
+			int a = sc.nextInt(); 
+			llist.addToTheLast(new Node(a));
+		}
+        	GfG g = new GfG(); 
+		System.out.println(g.getNthFromLast(llist.head,k));
+	    	t--;
+	}    
+   }
+	
 }
 
 class GfG{
@@ -84,7 +82,7 @@ class GfG{
             currentPosition--;
             iterator = iterator.next;
         }
-    	  return -1;
+    	return -1;
     	
     }
     
