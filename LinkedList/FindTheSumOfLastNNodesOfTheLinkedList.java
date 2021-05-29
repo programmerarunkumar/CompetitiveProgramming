f15d7d2a7efacc5e217354043fd2f443cddcde8f
Arun Kumar <58946415+programmerarunkumar@users.noreply.github.com>
2021-05-29 15:16:46 +0530

    Create FindTheSumOfLastNNodesOfTheLinkedList.java

---
+++ LinkedList/FindTheSumOfLastNNodesOfTheLinkedList.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    
+    public Node (int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class GFG {
+    
+	public static void main (String[] args) {
+		Scanner sc  = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- > 0){
+		    int n = sc.nextInt();
+		    int k = sc.nextInt();
+		    
+		    Node head = new Node(sc.nextInt());
+		    Node tail = head;
+		    
+		    for(int i=1; i<n; i++){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		    
+		    Solution obj = new Solution();
+		    System.out.println(obj.sum(head, k));
+		}
+	}
+	
+}
+
+class Solution {
+    
+    public int sum(Node head, int k){
+        
+        int sumOfLastNNodes = 0;
+        if(head == null){
+            return sumOfLastNNodes;
+        }
+        
+        int length = 0;
+        Node iterator = head;
+        while(iterator != null){
+            length++;
+            sumOfLastNNodes = sumOfLastNNodes + iterator.data;
+            iterator = iterator.next;
+        }
+        
+        iterator = head;
+        while(length != k){
+            sumOfLastNNodes = sumOfLastNNodes - iterator.data;
+            iterator = iterator.next;
+            length--;
+        }
+        return sumOfLastNNodes;
+     
+    }
+    
+}
