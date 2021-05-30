002abce3893fa39f5dc00edfb132f4bceea694fe
Arun Kumar <58946415+programmerarunkumar@users.noreply.github.com>
2021-05-30 20:03:29 +0530

    Create FindLengthOfLoop.java

---
+++ LinkedList/FindLengthOfLoop.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+}
+
+class GFG{
+        
+    public static void makeLoop(Node head, Node tail, int x){
+        if (x == 0) return;
+        
+        Node curr = head;
+        for(int i=1; i<x; i++)
+            curr = curr.next;
+        
+        tail.next = curr;
+    }
+    
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t--> 0)
+        {
+            int n = sc.nextInt();
+            
+            int num = sc.nextInt();
+            Node head = new Node(num);
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++)
+            {
+                num = sc.nextInt();
+                tail.next = new Node(num);
+                tail = tail.next;
+            }
+            
+            int pos = sc.nextInt();
+            makeLoop(head, tail, pos);
+            
+            Solution x = new Solution();
+            System.out.println( x.countNodesinLoop(head) );
+        }
+    }
+}
+
+class Solution{
+        
+    static int countNodesinLoop(Node head){
+        
+        int count = 0;
+        Node slowIterator = head;
+        Node fastIterator = head;
+        while(slowIterator != null && fastIterator.next != null && fastIterator.next.next != null){
+            slowIterator = slowIterator.next;
+            fastIterator = fastIterator.next.next;
+            if(slowIterator == fastIterator){
+                return countNodes(slowIterator);
+            }    
+                
+        }
+        return count;
+        
+    }
+    
+    static int countNodes(Node loopNode){
+        
+        int count = 1;
+        Node iterator = loopNode.next;
+        while(iterator != null){
+            if(iterator == loopNode){
+                break;
+            }
+            count++;
+            iterator = iterator.next;
+        }
+        return count;
+        
+    }
+    
+}
