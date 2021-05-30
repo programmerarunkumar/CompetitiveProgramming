9182b8a6dd5be5596e9fee5367d4fbd703fc86ad
Arun Kumar <58946415+programmerarunkumar@users.noreply.github.com>
2021-05-30 17:20:11 +0530

    Update FindNByKthNodeInLinkedList.java

--- LinkedList/FindNByKthNodeInLinkedList.java
+++ LinkedList/FindNByKthNodeInLinkedList.java
@@ -2,21 +2,21 @@ import java.util.*;
 
 class Node{
     
-    int data;
-    Node next;
+	int data;
+    	Node next;
     
-    Node(int key){
-        data = key;
-        next = null;
-    }
-    
+    	Node(int key){
+        	data = key;
+        	next = null;
+    	}
+
 }
 
 class LinkedList{
     
-    static Node head;
+    	static Node head;
     
-     public static void addToTheLast(Node node) {
+	public static void addToTheLast(Node node) {
 		if (head == null) 
 		{
 			head = node;
@@ -26,55 +26,56 @@ class LinkedList{
 		   temp = temp.next;
 		   temp.next = node;
 		}
-    }
+    	}
     
-    public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static void main (String[] args) {
+        	Scanner sc = new Scanner(System.in);
 		int t=sc.nextInt();
 		 
 		while(t-- > 0){
-		    int n = sc.nextInt();
-		    int a1 = sc.nextInt();
-		    Node head = new Node(a1);
-		    addToTheLast(head);
-		    for(int i = 1; i < n; i++){
-		        int a = sc.nextInt(); 
+			int n = sc.nextInt();
+		    	int a1 = sc.nextInt();
+		    	Node head = new Node(a1);
+		    	addToTheLast(head);
+		    	for(int i = 1; i < n; i++){
+		        	int a = sc.nextInt(); 
 				addToTheLast(new Node(a));
-			}
+		    	}
 			int k = sc.nextInt();
 			GfG gfg = new GfG();
 			System.out.println(gfg.nknode(head, k));
 		}
+		
 	}
 	
 }
 
 class GfG{
     
-    public static int nknode(Node head, int k){
+	public static int nknode(Node head, int k){
     
-        int length = 0;
-        Node iterator = head;
-        while(iterator != null){
-            iterator = iterator.next;
-            length++;
-        }
+        	int length = 0;
+        	Node iterator = head;
+       		while(iterator != null){
+            		iterator = iterator.next;
+            		length++;
+        	}
        
-        int nkNodeValue = -1;
-        int nkNodePosition = (length/k) + (length%k == 0 ? 0 : 1);
+        	int nkNodeValue = -1;
+        	int nkNodePosition = (length/k) + (length%k == 0 ? 0 : 1);
        
-        iterator = head;
-        int currentPosition = 1;
-        while(iterator != null){
-            if(currentPosition == nkNodePosition){
-                nkNodeValue = iterator.data;
-                break;
-            }
-            currentPosition++;
-            iterator = iterator.next;
-        }
-        return nkNodeValue;
+        	iterator = head;
+        	int currentPosition = 1;
+        	while(iterator != null){
+            		if(currentPosition == nkNodePosition){
+                		nkNodeValue = iterator.data;
+                		break;
+            		}
+            		currentPosition++;
+            		iterator = iterator.next;
+        	}
+        	return nkNodeValue;
         
-    }
+    	}
     
 }
